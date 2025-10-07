bfc1463e76709279995b3473a7195288403ba57a
yashdev9274 <yashdev.yvd@gmail.com>
2025-10-07 18:03:00 +0530

    fix

--- apps/web/src/components/ui/footer.tsx
+++ apps/web/src/components/ui/footer.tsx
@@ -6,10 +6,10 @@ export default function FooterSection() {
          <div className="h-auto p-4 md:p-8 flex flex-col justify-start items-start gap-8">
            {/* Brand Section */}
            <div className="self-stretch flex justify-start items-center gap-3">
-             <div className="text-center text-[#49423D] text-xl font-semibold leading-4 font-sans">Brillance</div>
+             <div className="text-center text-[#49423D] text-xl font-semibold leading-4 font-sans">Devdocs</div>
            </div>
            <div className="text-[rgba(73,66,61,0.90)] text-sm font-medium leading-[18px] font-sans">
-             Coding made effortless
+             Documentation made eazieeee
            </div>
  
            {/* Social Media Icons */}
